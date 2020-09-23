c69e85d582b793b5aa202b28edeb01edb2490ccc
hong <417670161@qq.com>
2020-09-23 22:34:18 +0800

    防止缓存击穿.

--- FishCache/fishcache.go
+++ FishCache/fishcache.go
@@ -1,11 +1,14 @@
 package FishCache
 
 import (
+	"FishCache/singleflight"
 	"fmt"
 	"log"
 	"sync"
 )
 
+//定义一个函数类型 F，并且实现接口 A 的方法，然后在这个方法中调用自己。
+//这是 Go 语言中将其他函数（参数返回值定义与 F 一致）转换为接口 A 的常用技巧。
 // A Getter loads data for a key.
 type Getter interface {
 	Get(key string) ([]byte, error)
@@ -24,6 +27,9 @@ type Group struct {
 	getter    Getter
 	mainCache cache
 	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
 }
 
 var (
@@ -31,7 +37,7 @@ var (
 	groups = make(map[string]*Group)
 )
 
-func NewGroup(name string,cacheSize int64,getter Getter)*Group {
+func NewGroup(name string, cacheSize int64, getter Getter) *Group {
 	if getter == nil {
 		panic("nil Getter")
 	}
@@ -41,6 +47,7 @@ func NewGroup(name string,cacheSize int64,getter Getter)*Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheSize: cacheSize},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -65,7 +72,6 @@ func (g *Group) Get(key string) (ByteView, error) {
 		log.Println("[FishCache] hit")
 		return v, nil
 	}
-
 	return g.load(key)
 }
 
@@ -78,15 +84,23 @@ func (g *Group) RegisterPeers(peers PeerPicker) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[FishCache] Failed to get from peer", err)
 			}
-			log.Println("[FishCache] Failed to get from peer", err)
 		}
+		return g.getLocally(key)
+	})
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
-	return g.getLocally(key)
+	return
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
@@ -111,4 +125,3 @@ func (g *Group) getLocally(key string) (ByteView, error) {
 func (g *Group) populateCache(key string, value ByteView) {
 	g.mainCache.add(key, value)
 }
-
